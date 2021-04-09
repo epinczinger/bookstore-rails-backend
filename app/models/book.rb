8f015abd65cf639b327c70db36cc843db021f24d
EstebanP <epinczinger@gmail.com>
2021-04-09 12:45:48 -0300

    add associations between models, fix bookid on mig

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+    belongs_to :user
+    belongs_to :category
 end
--- db/migrate/20210409153450_create_books.rb
+++ db/migrate/20210409153450_create_books.rb
@@ -1,7 +1,6 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
-      t.integer :id
       t.string :title
       t.string :author
       t.string :progress
