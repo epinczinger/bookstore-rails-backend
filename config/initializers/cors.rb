52cb39af8ba4457171632d331eebbf22aefa35b5
EstebanP <epinczinger@gmail.com>
2021-04-15 10:20:51 -0300

    expose acces-token when request it from frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :delete]
+    resource '*', headers: :any, methods: [:get, :post, :delete], expose: ['*']
   end
 end
