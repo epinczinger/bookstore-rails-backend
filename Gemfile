7a33b0398490289d879793a45b27b675a2b1cb40
EstebanP <epinczinger@gmail.com>
2021-04-16 19:28:28 -0300

    change db to postgresql

--- Gemfile
+++ Gemfile
@@ -3,13 +3,14 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.0'
 
+gem 'pg'
 gem 'activerecord-reset-pk-sequence'
 gem 'rack-cors'
 gem 'hirb'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.4'
+# gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_09_153952) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -20,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_04_09_153952) do
     t.string "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["id"], name: "index_books_on_id", unique: true
   end
 
   create_table "categories", force: :cascade do |t|
